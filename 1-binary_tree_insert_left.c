d9243b9fda8be97327fa60a784d570868d75aa0f
Bram-Wel <welbram@gmail.com>
2023-02-07 03:07:44 +0300

    Betty: Trailing statements on next line

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (node);
 	}
 
-	if (node) free(node);
+	if (node)
+		free(node);
 
 	return (0); /*Return NULL*/
 }
