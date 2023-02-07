2e42c1bcf6421e05ffa3c96f58333b65e0831f7c
Bram-Wel <welbram@gmail.com>
2023-02-07 04:15:22 +0300

    Check `leafity` Hhe ;)

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_leaf - Check that a node is a leaf
+*
+* @node: Pointer to node
+* Return: 1 for leaf node, 0 otherwise
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	int retval = 0;
+
+	if (node && !node->left && !node->right)
+		retval = 1;
+	return retval;
+
+}
