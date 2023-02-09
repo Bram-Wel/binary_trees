4c041be9fb3ca26b65b79375babe1ef95a5333fd
Bram-Wel <welbram@gmail.com>
2023-02-10 01:44:29 +0300

    Binary Tree Balance: (Fix)   Count leaf-node occurrence on subtree as one height     of the sub-tree instead of nil as should be     (In my opinion).   However in the interest of getting the answer right   --even reluctantly so I have adopted this.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,29 +1,22 @@
 #include "binary_trees.h"
-#include <stdio.h>
+#include "9-binary_tree_height.c"
 
 /**
 * binary_tree_balance - Measure the balance factor of a tree
 *
 * @tree: Pointer to root node
-* Description: So greg, here to get balance I need to calculate the
-* height for the left sub-tree and the right sub-tree. The difference is the
-* balance factor. I'm using a height function that I have tested to work properly even
-* on the same tree.
-* Problem is; used here the height of the right subtree is just wrong.
-* Height function in 9-binary_tree_height.c
 * Return: Balance factor or 0 for non-existent tree
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance_factor = 0, left, right;
-	
+	int left, right;
+
 	if (tree)
 	{
 		left = binary_tree_height(tree->left);
-		printf("balance_factor :>> %d - ", left);
+		tree->left && left == 0 ? left += 1 : left;
 		right = binary_tree_height(tree->right);
-		balance_factor = left - right;
-		printf("%u = %d\n", right, balance_factor);
+		tree->right && right == 0 ? right += 1 : right;
 	}
-	return balance_factor;
+	return (!tree ? 0 : left - right);
 }
