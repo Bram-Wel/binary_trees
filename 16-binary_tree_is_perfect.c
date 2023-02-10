82e5ba1c101b5b8bd3d03f32e55f031d6713db06
Bram-Wel <welbram@gmail.com>
2023-02-10 21:28:48 +0300

    Perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 #include "14-binary_tree_balance.c"
 
+int recursive_balance(const binary_tree_t *tree);
 /**
 * binary_tree_is_perfect - Check that a binary tree is perfect
 *
@@ -12,9 +13,27 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int retval = 0;
 
 	if (tree)
-	{
-		if (binary_tree_balance(tree) == 0)
-			retval = 1;
-	}
+		recursive_balance(tree) == 0 ? retval = 1 : retval;
 	return (retval);
+}
+
+/**
+* recursive_balance - run balance recursively
+*
+* @tree: Root node
+* Return: Total balance for the tree.
+*/
+int recursive_balance(const binary_tree_t *tree)
+{
+	int balance = 0;
+
+	if (tree)
+	{
+		balance = binary_tree_balance(tree);
+		if (balance != 0)
+			return (balance);
+		balance = recursive_balance(tree->left) ||
+			recursive_balance(tree->right);
+	}
+	return (balance);
 }
